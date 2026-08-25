cde454b89f7518c29449b22701ba7bc27ccabdb3
Rudra Pratap Singh <rudrapratapsingh.cse@gmail.com>
2026-08-25 10:16:33 +0000

    JS basic

---
+++ .devcontainer/test.js
@@ -0,0 +1,33 @@
+const roll = 11018
+var name = "Rudra Pratap Singh" // prefer not to use var 
+// //reason : issue in block scope & function scope {}
+// // let Sport = "Cricket"
+// let category = "Batsman"
+// console.log(roll)
+// console.table([name,Sport,category])
+
+"use strict"; // newer version of JS
+// alert (3+3) // we're using nodejs , not browser 
+// in developer console it pop's out 6
+
+ // console.log(typeof "Rudra ") // return the type 
+// console.log(typeof null) //object or ...
+// console.log(typeof undefined) // undefined 
+
+// let score = 33
+// let negScore = - score
+// console.log(negScore)
+
+// console.table([1+1,1+"1",1+1+"1","1"+1+1])
+// output: 
+// ├─────────┼────────┤
+// │ 0       │ 2      │
+// │ 1       │ '11'   │
+// │ 2       │ '21'   │
+// │ 3       │ '111'  │
+
+console.log(+true); // 1
+console.log(-true); // 1
+
+console.log(+" ");
+console.log(-" ");
